2a567bcad059fe909c8f23a64b91b581e033a84a
lishaohua <lshstyle@163.com>
2019-03-22 12:28:29 +0800

    git

---
+++ rabbitmq-product/src/main/java/com/kingdee/controller/ProductController.java
@@ -0,0 +1,42 @@
+package com.kingdee.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kingdee.service.ProductService;
+
+@RestController
+@RequestMapping("/product")
+public class ProductController {
+	
+	@Autowired
+	private ProductService productService;
+	
+	@GetMapping(value = "/fanout")
+	public void fanout( @RequestParam("msg") String msg) {
+		productService.fanout(msg);
+		
+	}
+	
+	@GetMapping(value = "/deadFanout")
+	public void deadFanout( @RequestParam("msg") String msg) {
+		productService.deadFanout(msg);
+		
+	}
+	
+	@GetMapping(value = "/topic")
+	public void topic( @RequestParam("msg") String msg) {
+		productService.topic(msg);
+		
+	}
+	
+	@GetMapping(value = "/direct")
+	public void direct( @RequestParam("msg") String msg) {
+		productService.direct(msg);
+		
+	}
+
+}
